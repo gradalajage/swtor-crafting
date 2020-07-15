9069505bf1f0970985a2f005d6eccd48847f5c46
gradalajage <gradalajage@example.com>
2020-07-14 21:21:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwtorCrafting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
